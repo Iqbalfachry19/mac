b68e183e88af719a3f910a3bf3fcfae97462396c
iqbal <ifachry@rocketmail.com>
2023-07-30 11:53:18 +0700

    Initial Commit

---
+++ mac/macApp.swift
@@ -0,0 +1,17 @@
+//
+//  macApp.swift
+//  mac
+//
+//  Created by Iqbal Fachry on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
